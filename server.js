057b5fb9e3e067c64427439fb6b0795251b58ffd
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-04-14 10:13:26 -0500

    additional set up. Added folders and base CSS files

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
@@ -12,10 +15,6 @@ app.get('/', (request, response) => {
   response.render('index');
 });
 
-
-
-const cors = require('cors');
-app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
