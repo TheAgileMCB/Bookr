814f559d568aa31f96dade326efa0de1f1fe4246
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-04-14 12:44:06 -0500

    built and connected bookHandler fuction. Working on API request

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
 
+// required middleware and modules
 require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bookHandler = require('./modules/books');
+
+// set up apps and EJS
 app.use(cors());
 
 app.set('view engine', 'ejs');
@@ -15,11 +19,15 @@ app.get('/', (request, response) => {
   response.render('pages/index');
 });
 
+//renders book search form
 app.get('/searches/new', (request, response) => {
   response.render('pages/searches/new');
 });
 
+//renders response of bookHandler
+app.post('/searches', bookHandler);
 
+// establish server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
 console.log(`heard on ${PORT}`));
