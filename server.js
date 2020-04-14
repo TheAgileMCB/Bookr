2b93a63b327bcdae3e451c1de419b892a4c70221
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-04-14 10:45:36 -0500

    paNew gese populated with search form and form is linked in server

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/', (request, response) => {
   response.render('pages/index');
 });
 
+app.get('/searches/new', (request, response) => {
+  response.render('pages/searches/new');
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
