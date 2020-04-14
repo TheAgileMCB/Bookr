c178a3830de05b59e9761d9fad6ce000b6ba9ee6
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-04-14 10:26:51 -0500

    organized folders, including references to CSS files

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
 app.get('/', (request, response) => {
-  response.render('index');
+  response.render('pages/index');
 });
 
 
